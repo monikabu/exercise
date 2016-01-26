172bc29b293700e98cfad9d96df8d8ca97d0528a
monikabu <mna.burdzy@gmail.com>
2016-01-26 16:59:25 +0100

    Use shared examples in specs

--- spec/roman_numeral_spec.rb
+++ spec/roman_numeral_spec.rb
@@ -1,60 +1,59 @@
 require 'spec_helper'
+shared_examples_for 'validable' do |number|
+  it "raises an error for #{number}" do
+    expect { described_class.roman_representation_of number }.
+      to raise_error(ArgumentError, /must be a positive integer/)
+  end
+end
+
+shared_examples_for 'transformable' do |number, roman_numeral|
+  it "returns #{roman_numeral} for #{number}" do
+    expect(described_class.roman_representation_of number).
+      to eq roman_numeral
+  end
+end
 
 describe RomanNumeral do
   describe '.roman_representation_of' do
     context 'Invalid Argument' do
       context 'Not a number' do
-        it 'returns a warning message' do
-          expect { described_class.roman_representation_of('not a number') }.
-            to raise_error(ArgumentError,/must be.*integer/)
-        end
+        it_should_behave_like 'validable', 'not a number'
       end
 
-      context 'Negative number' do
-        it 'returns a warning message' do
-          expect { described_class.roman_representation_of(-3) }.
-            to raise_error(ArgumentError, /must be a positive integer/)
-        end
+      context 'negative number' do
+        it_should_behave_like 'validable', -3
       end
 
-      context 'Number is 0' do
-        it 'returns a warning message' do
-          expect { described_class.roman_representation_of(0) }.
-            to raise_error(ArgumentError, /must be a positive integer/)
-        end
+      context 'number is 0' do
+        it_should_behave_like 'validable', 0
       end
 
-      context 'Number is higher than 10_000' do
-        it 'returns a warning message' do
-          expect { described_class.roman_representation_of(10_000) }.
-            to raise_error(ArgumentError, /must be.*lower than 10 000/)
-        end
+      context 'to high number' do
+        it_should_behave_like 'validable', 10_000
       end
     end
 
     context 'Valid number' do
-      it 'returns roman representation of an integer' do
-        expect(described_class.roman_representation_of(1)).to eq('I')
-        expect(described_class.roman_representation_of(2)).to eq('II')
-        expect(described_class.roman_representation_of(4)).to eq('IV')
-        expect(described_class.roman_representation_of(5)).to eq('V')
-        expect(described_class.roman_representation_of(10)).to eq('X')
-        expect(described_class.roman_representation_of(12)).to eq('XII')
-        expect(described_class.roman_representation_of(24)).to eq('XXIV')
-        expect(described_class.roman_representation_of(50)).to eq('L')
-        expect(described_class.roman_representation_of(54)).to eq('LIV')
-        expect(described_class.roman_representation_of(68)).to eq('LXVIII')
-        expect(described_class.roman_representation_of(92)).to eq('XCII')
-        expect(described_class.roman_representation_of(103)).to eq('CIII')
-        expect(described_class.roman_representation_of(112)).to eq('CXII')
-        expect(described_class.roman_representation_of(498)).to eq('CDXCVIII')
-        expect(described_class.roman_representation_of(544)).to eq('DXLIV')
-        expect(described_class.roman_representation_of(1000)).to eq('M')
-        expect(described_class.roman_representation_of(1024)).to eq('MXXIV')
-        expect(described_class.roman_representation_of(2094)).to eq('MMXCIV')
-        expect(described_class.roman_representation_of(5944)).to eq('MMMMMDMXLIV')
-        expect(described_class.roman_representation_of(9999)).to eq('MMMMMMMMMDMXCIX')
-      end
+      it_should_behave_like 'transformable', 1, 'I'
+      it_should_behave_like 'transformable', 2, 'II'
+      it_should_behave_like 'transformable', 4, 'IV'
+      it_should_behave_like 'transformable', 5, 'V'
+      it_should_behave_like 'transformable', 10, 'X'
+      it_should_behave_like 'transformable', 12, 'XII'
+      it_should_behave_like 'transformable', 24, 'XXIV'
+      it_should_behave_like 'transformable', 50, 'L'
+      it_should_behave_like 'transformable', 54, 'LIV'
+      it_should_behave_like 'transformable', 68, 'LXVIII'
+      it_should_behave_like 'transformable', 92, 'XCII'
+      it_should_behave_like 'transformable', 103, 'CIII'
+      it_should_behave_like 'transformable', 112, 'CXII'
+      it_should_behave_like 'transformable', 498, 'CDXCVIII'
+      it_should_behave_like 'transformable', 544, 'DXLIV'
+      it_should_behave_like 'transformable', 1000, 'M'
+      it_should_behave_like 'transformable', 1024, 'MXXIV'
+      it_should_behave_like 'transformable', 2094, 'MMXCIV'
+      it_should_behave_like 'transformable', 5944, 'MMMMMDMXLIV'
+      it_should_behave_like 'transformable', 9999, 'MMMMMMMMMDMXCIX'
     end
   end
 end
